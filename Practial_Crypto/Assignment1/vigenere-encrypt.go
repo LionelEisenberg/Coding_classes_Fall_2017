04e46a5dc64b779a3d2a6438313c496f472e0cc3
LIONEL-PC\Lionel <lionel.eisenberg@gmail.com>
2017-09-05 11:52:52 -0400

    utd

--- Practial_Crypto/Assignment1/vigenere-encrypt.go
+++ Practial_Crypto/Assignment1/vigenere-encrypt.go
@@ -1,27 +1,60 @@
 package main
 
 import (
-    "os"
-    //"io"
-    //"io/ioutil"
-    "fmt"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
 )
 
-func checkArgs() bool{
-    if (len(os.Args) != 3) {
-        return false
-    }
-    return true
+func checkArgs() bool {
+	if len(os.Args) != 3 {
+		panic("THe number of arguments is incorrect")
+	}
+	return true
+}
+
+func checkForFile(err error) {
+	if err != nil {
+		panic("the name of the file is incorrect")
+	}
+}
+
+func checkRegErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func checkForKeySize(key string) {
+	if len(key) > 32 {
+		panic("key is too big, please try with a key smaller than 32 characters")
+	}
 }
 
 func main() {
-    if (!checkArgs()) {
-        panic("Wrong number of command line arguments");
-    }
+	checkArgs()
 
-    var key, fileName string
-    key = os.Args[1];
-    fileName = os.Args[2]
+	var key, fileName string
+	key = strings.ToUpper(os.Args[1])
+	fileName = os.Args[2]
 
-    fmt.Println("the key is ", key, " and the file name is ", fileName);
+	fileContent, err := ioutil.ReadFile(fileName)
+	checkForFile(err)
+	checkForKeySize(key)
+	fmt.Println(fileContent)
+	fmt.Println(key)
+
+	regExp, err := regexp.Compile("[^a-zA-Z]+")
+	checkRegErr(err)
+	processedString := strings.ToUpper(regExp.ReplaceAllString(string(fileContent), ""))
+	fmt.Println(processedString)
+
+	m := make(map[int]string)
+
+	for i := range []byte(processedString) {
+
+	}
+
 }
