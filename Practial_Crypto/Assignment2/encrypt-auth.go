486602bc283d74cb2f8b9b3519796848b24242fb
Lionel Eisenberg <lionel.eisenberg@gmail.com>
2017-09-19 00:04:00 -0400

    started assignment 2 of crypto

---
+++ Practial_Crypto/Assignment2/encrypt-auth.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+  "fmt"
+  "os" //Package os provides a platform-independent interface to operating system functionality.
+  "encoding/hex"
+  //"reflect"
+  // "io"
+  // "io/ioutil" //For File Reading
+)
+
+func checkArgs() {
+  if len(os.Args) != 8 {
+    panic("Number of arguments is incorrect")
+  } else if os.Args[1] != "encrypt" && os.Args[1] != "decrypt" {
+    panic("Unrecognized mode");
+  } else if os.Args[2] != "-k" {
+    panic("unrecognized key flag");
+  } else if os.Args[4] != "-i" {
+    panic("unrecognized input flag");
+  } else if os.Args[6] != "-o" {
+    panic("unrecgnized output flag");
+  }
+}
+
+func checkKey() {
+  key := os.Args[3];
+  if len(key) != 64 {
+    panic("Length of inputed key is less than 32.")
+  }
+}
+
+func checkForError(err error) {
+  if err != nil {
+    panic(err)
+  }
+}
+
+func main() {
+  checkArgs();
+  checkKey();
+  keyAndMAC, err := hex.DecodeString(os.Args[3])
+  checkForError(err);
+  key := keyAndMAC[0:16];
+  MAC := keyAndMAC[16:32]
+
+
+  fmt.Println(keyAndMAC);
+  fmt.Println(key, "\n", MAC);
+}
